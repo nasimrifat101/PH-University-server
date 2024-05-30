523a6445174a91f89b6caeb7abb702edd6850e33
nasimrifat101 <nasimrifat101@gmail.com>
2024-05-30 20:19:45 +0600

    [user] validation schema interface created

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,11 +2,7 @@ import { z } from "zod";
 
 const userValidationSchema = z.object({
   id: z.string(),
-  password: z.string().max(20).min(8),
-  needsPasswordChange: z.boolean().optional().default(true),
-  role: z.enum(["admin", "student", "faculty"]),
-  status: z.enum(["in-progress", "block"]).default("in-progress"),
-  isDeleted: z.boolean().default(false),
+  password: z.string().max(20).min(8).optional(),
 });
 
 export const userValidation = {
