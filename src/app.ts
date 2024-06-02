ffd0840c7bc2d63200cb44e5d29cdcc61fbc099f
nasimrifat101 <nasimrifat101@gmail.com>
2024-06-02 20:54:22 +0600

    kkf

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import { studentRoutes } from "./app/modules/student/student.route";
 import { userRoutes } from "./app/modules/user/user.routes";
+
 const app: Application = express();
 
 app.use(express.json());
@@ -14,5 +15,22 @@ app.get("/", (req: Request, res: Response) => {
 // Applications
 app.use("/api/v1/students", studentRoutes);
 app.use("/api/v1/users", userRoutes);
+
+// Error-handling middleware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  let statusCode = 500;
+  let message = "Internal Server Error";
+
+  if (err) {
+    message = err.message;
+    statusCode = err.status || 500;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    data: err,
+  });
+});
 
 export default app;
--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { studentServices } from "./student.service";
 
-
-
-
-const getAllStudents = async (req: Request, res: Response) => {
+const getAllStudents = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const result = await studentServices.getAllStudentFromDB();
     res.status(200).json({
@@ -13,15 +14,15 @@ const getAllStudents = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message || "Student retrieve failed",
-      data: err,
-    });
+    next(err);
   }
 };
 
-const getSingleStudent = async (req: Request, res: Response) => {
+const getSingleStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { studentId } = req.params;
     const result = studentServices.getSingleStudentFromDB(studentId);
@@ -31,15 +32,15 @@ const getSingleStudent = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message || "Student retrieve failed",
-      data: err,
-    });
+    next(err);
   }
 };
 
-const deleteStudent = async (req: Request, res: Response) => {
+const deleteStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { studentId } = req.params;
     const result = studentServices.deleteStudentFromDB(studentId);
@@ -49,11 +50,7 @@ const deleteStudent = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message || "Student delete failed",
-      data: err,
-    });
+    next(err);
   }
 };
 
